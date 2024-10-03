7f855a9e7fa17cb3cd80f19d1f1961df6d02dcf8
sunflow-er <antjd03170@gmail.com>
2024-10-03 18:51:58 +0900

    Material Design

--- app/src/main/java/org/javaapp/jetpackcompose/MainActivity.kt
+++ app/src/main/java/org/javaapp/jetpackcompose/MainActivity.kt
@@ -1,19 +1,24 @@
 package org.javaapp.jetpackcompose
 
+import android.content.res.Configuration
 import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,6 +26,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.javaapp.jetpackcompose.ui.theme.JetpackComposeTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +35,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            JetpackComposeTheme { // Material Theme
+                Surface(modifier = Modifier.fillMaxSize()) { // Surface
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
+
         }
     }
 }
@@ -40,31 +51,61 @@ data class Message(val author: String, val body: String)
 fun MessageCard(msg: Message) {
     // Add padding around our message
     Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image (
+        Image(
             painter = painterResource(id = R.drawable.baseline_account_circle_24),
             contentDescription = "Contact profile picture",
             modifier = Modifier
                 .size(40.dp) // Set image size to 40dp
                 .clip(CircleShape) // Clip image to be shaped as a Circle
+                .border(
+                    1.5.dp,
+                    MaterialTheme.colorScheme.primary,
+                    CircleShape
+                ) // Material Design - Color
         )
 
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary, // Material Design - Color
+                style = MaterialTheme.typography.titleSmall // Material Design - Typography
+            )
             Spacer(modifier = Modifier.height(4.dp)) // Add a vertical space between the author and message texts
-            Text(text = msg.body)
+
+            Surface (shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) { // Material Design - Shape
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
         }
     }
 }
 
-@Preview // 에뮬레이터가 아닌 안드로이드 스튜디오 내에서 미리 볼 수 있음, 매개변수가 없는 Composable 함수에 사용 가능
+
+@Preview(name = "Light Mode") // 에뮬레이터가 아닌 안드로이드 스튜디오 내에서 미리 볼 수 있음, 매개변수가 없는 Composable 함수에 사용 가능
+@Preview (
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        Message(
-            "Lexi", "Hey, take a look at Jetpack Compose, it's great!"
-        )
-    )
+    JetpackComposeTheme {
+        Surface {
+            MessageCard(
+                Message(
+                    "Lexi", "Hey, take a look at Jetpack Compose, it's great!"
+                )
+            )
+        }
+    }
+
 }
+
+
